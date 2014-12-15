193bdb09e5f4eb548e241d8d59971d19a3a8ec2e
yubaoquan <1079139460@qq.com>
2014-12-15 20:12:53 +0800

    20141215

--- ybqApp/public/javascripts/controller.js
+++ ybqApp/public/javascripts/controller.js
@@ -7,7 +7,7 @@ var menziApp = angular.module('menziApp', []);
 //   console.log('here2333');
 // });
 menziApp.controller('menziController', ['$scope', '$http', function ($scope, $http) {
-  $http.get('data/a.json').success(function (data) {
+  $http.get('/data/a.json').success(function (data) {
     $scope.uPeiliaos = data;
   });
   $scope.gay = {
