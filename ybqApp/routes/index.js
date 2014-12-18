ad24c11e292e42d37efac4dbe2405bf8ace86574
yubaoquan <721731428@qq.com>
2014-12-18 15:36:37 +0800

    .
    header从html替换成jade

--- ybqApp/routes/index.js
+++ ybqApp/routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res) {
   console.log('abcccccd');
-  res.render('index2', { title : '闷子聊天模拟器' });
+  res.render('index2', { 
+    title : '闷子聊天模拟器',
+    testdata : 'this is test data'
+  });
 });
 
 
