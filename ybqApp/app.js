2fab89b5bd0e454b6a06f4383e66cda187be4ecb
yubaoquan <721731428@qq.com>
2014-12-19 18:25:44 +0800

    路由

--- ybqApp/app.js
+++ ybqApp/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-console.log('routes');
 var users = require('./routes/users');
 
 var app = express();
@@ -24,14 +23,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
 
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+routes(app);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -39,28 +53,11 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: err
     });
-});
-
-routes(app);
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-// module.exports = app;
+  });
+}
--- ybqApp/routes/index.js
+++ ybqApp/routes/index.js
@@ -1,25 +1,10 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function (req, res) {
-//   console.log('abcccccd');
-//   res.render('index2', { 
-//     title : '闷子聊天模拟器',
-//     testdata : 'this is test data'
-//   });
-// });
-
-
-
-// module.exports = router;
 module.exports = function (app) {
-    app.get('/', function (req, res) {
-    console.log('abcccccd');
-    res.render('index2', { 
+  console.log('I am routes');
+  app.route('/').get(function (req, res, next) {
+    console.log('get');
+    res.render('index2', {
       title : '闷子聊天模拟器',
       testdata : 'this is test data'
     });
   });
-
 };
